30ef812889072fca9299560a42e3c382fddc0405
lupantech <lupantech@gmail.com>
2025-02-21 14:26:21 -0800

    fixed tools

--- tools/path_generalist_classifier/data.js
+++ tools/path_generalist_classifier/data.js
@@ -1,4 +1,4 @@
-const global_tool_name = "🏥 PubMed Paper Search";
-const global_tool_description = "Search PubMed for the latest literature based on a given query.";
+const global_tool_name = "🔬 Pathology Classifier";
+const global_tool_description = "Classify H&E-stained pathology images into one of the given options.";
 
 export { global_tool_name, global_tool_description };
