11a6775d063bdac2be3b9b3a4d26333b3530c8a6
IsaacGHX <Isaac_GHX@outlook.com>
2025-10-01 16:32:19 +0800

    tools and cases, info updated

--- tools/path_generalist_classifier/data.js
+++
@@ -1,4 +0,0 @@
-const global_tool_name = "🔬 Pathology Classifier";
-const global_tool_description = "Classify H&E-stained pathology images into one of the given options.";
-
-export { global_tool_name, global_tool_description };
